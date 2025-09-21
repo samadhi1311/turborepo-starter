edbdb32613ba8d3f62f858cdd7e89cc945578007
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-09-21 11:27:22 +0530

    chore: format code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@repo/ui/components/ui/button';
 
 export default function Home() {
     return (
-        <div className="flex h-screen items-center justify-center">
+        <div className='flex h-screen items-center justify-center'>
             <Button>Shadcn Ui</Button>
         </div>
     );
--- packages/ui/README.md
+++ packages/ui/README.md
@@ -41,7 +41,7 @@ import { Button } from '@repo/ui/components/ui/button';
 export default function Home() {
     return (
         <div>
-            <h1 className="text-3xl text-red-500">Test</h1>
+            <h1 className='text-3xl text-red-500'>Test</h1>
             <Button>Test</Button>
         </div>
     );
