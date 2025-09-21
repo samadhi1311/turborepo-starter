7f95560aabed0e56ed9c70beaf2b2fb6cb8a0938
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-09-21 12:04:30 +0530

    chore: add landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,43 @@
+'use client';
+
 import { Button } from '@repo/ui/components/ui/button';
+import { ExternalLinkIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
 
 export default function Home() {
+    const { theme } = useTheme();
     return (
-        <div className='flex h-screen items-center justify-center'>
-            <Button>Shadcn Ui</Button>
+        <div className='flex flex-col gap-8 items-center justify-center w-screen h-screen bg-background'>
+            <div>
+                <img
+                    src={`${theme === 'dark' ? '/turborepo-dark.svg' : '/turborepo-light.svg'}`}
+                    className='h-16'
+                    alt='Turborepo Logo'
+                />
+            </div>
+
+            <div className='max-w-prose text-center'>
+                <p>
+                    A starter template using Next.JS frontend, Nest.JS backend
+                    and PostgresSQL database with Drizzle ORM
+                </p>
+            </div>
+
+            <div>
+                <Button variant='secondary' asChild>
+                    <Link
+                        href='https://github.com/samadhi1311/turborepo-starter'
+                        target='_blank'
+                    >
+                        <ExternalLinkIcon
+                            className='size-4'
+                            strokeWidth={2.5}
+                        />
+                        Learn More
+                    </Link>
+                </Button>
+            </div>
         </div>
     );
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['@repo/ui'],
+    output: 'export',
 };
 
 export default nextConfig;
