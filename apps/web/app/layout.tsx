6b5caab7b2ec20039c22de0030d5936618a9f5e2
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-09-21 12:04:20 +0530

    chore: update root layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,37 @@
 import './globals.css';
+import { ThemeProvider } from '@/components/theme/theme-provider';
+import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+    subsets: ['latin'],
+    style: 'normal',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    display: 'auto',
+    fallback: ['Helvetica', 'Segoe UI', 'sans-serif'],
+    preload: true,
+});
+
+export const metadata: Metadata = {
+    title: 'Turborepo Starter',
+    description:
+        'A starter template using Next.JS frontend, Nest.JS backend and PostgresSQL database with Drizzle ORM.',
+};
 
 export default function RootLayout({
     children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang='en'>
-            <body>{children}</body>
+        <html lang='en' suppressHydrationWarning>
+            <body className={`${inter.className} antialiased`}>
+                <ThemeProvider
+                    defaultTheme='system'
+                    enableSystem
+                    attribute='class'
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
